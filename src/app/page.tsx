3c71e30d7775b39385ebddfc9a30a34b68b67f83
Glynn Jordan <glynnjordan.jr@gmail.com>
2024-11-10 06:52:48 -0600

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Metadata } from "next";
 import Home from "./components/Home/Home";
 
 export const metadata: Metadata = {
-  title: "MadeTrade Ethical Product Search",
-  description: "Discover ethical, sustainable products with AI-powered search on MadeTrade.",
+  title: "ChatGPT Search",
+  description: "Proof of concept for integrating ChatGPT and LaunchDarkly",
 };
 
 const Page = () => {
